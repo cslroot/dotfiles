dad66b5ae658607ec0b4f7fa2674f012b6e0012c
TAMURA Takashi <cslroot@gmail.com>
2012-09-03 16:15:18 +0900

    append gitconfig and gitignore

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 git submodule init
 git submodule update
 
-for dotfile in .vimrc .bashrc .bash_profile
+for dotfile in .vimrc .bashrc .bash_profile .gitconfig .gitignore
 do
   rm -fr ~/$dotfile
   ln -vnfs $PWD/$dotfile ~/$dotfile
