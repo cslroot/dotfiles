da820cd7d117ae1b2a31c3c9fdfe9785d61f8b23
TAMURA Takashi <cslroot@gmail.com>
2015-08-30 00:31:24 +0900

    astyle設定ファイルを追加

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 git submodule init
 git submodule update
 
-for dotfile in .vimrc .bashrc .bash_profile .gitconfig .gitignore
+for dotfile in .vimrc .bashrc .bash_profile .gitconfig .gitignore .astylerc
 do
   rm -fr ~/$dotfile
   ln -vnfs $PWD/$dotfile ~/$dotfile
